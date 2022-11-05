239fb98974aa7a051f46552bbe97c63323fbc870
priority <75966543+priority3@users.noreply.github.com>
2022-11-05 21:52:11 +0800

    feat: namespace tool (#36)
    * fix: cli icons
    
    * feat: namespace bem

---
+++ packages/components/icons/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '@fuzzy/utils'
+import Icons from './src/icons.vue'
+
+export const FIcons = withInstall(Icons)
+
+export default FIcons
---
+++ packages/components/icons/src/icons.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'FIcons',
+})
+</script>
+
+<template>
+  <div class="icons">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
